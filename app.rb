4aec79fca3486e6ae19739199a79efab0571eef1
Kushal Pisavadia <kushi.p@gmail.com>
2016-02-26 16:15:45 +0000

    Use Random over SecureRandom

--- app.rb
+++ app.rb
@@ -1,9 +1,12 @@
-require 'securerandom'
 require 'sinatra'
 
 class TestHutchThreadsApp < Sinatra::Base
+  def initialize
+    super
+  end
+
   get '/' do
-    random_number = SecureRandom.random_number
+    random_number = Random.rand(1000)
     "Random number: #{random_number}"
   end
 end
