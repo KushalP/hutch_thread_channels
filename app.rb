dc4ab9fa797a3fbd916e2e121c7afed624c846d4
Kushal Pisavadia <kushi.p@gmail.com>
2016-02-26 16:34:18 +0000

    On requests, publish message through Hutch
    Whenever we receive a `GET` request, publish a message to RabbitMQ using
    Hutch defaults.

--- app.rb
+++ app.rb
@@ -1,12 +1,23 @@
+require 'hutch'
 require 'sinatra'
 
 class TestHutchThreadsApp < Sinatra::Base
+  PREFIX = "hutch_thread_channels"
+  QUEUE_NAME = "#{PREFIX}_queue"
+  ROUTING_KEY = "#{PREFIX}_routing_key"
+
   def initialize
+    Hutch.connect
+
+    queue = Hutch.broker.queue(QUEUE_NAME)
+    Hutch.broker.bind_queue(queue, [ROUTING_KEY])
+
     super
   end
 
   get '/' do
     random_number = Random.rand(1000)
+    Hutch.publish(ROUTING_KEY, subject: "random_number", action: random_number)
     "Random number: #{random_number}"
   end
 end
